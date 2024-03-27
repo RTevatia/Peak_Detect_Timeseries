35cb78ab1d42b8712cb617487ea38ba99a8f2588
Tevatia <rtevatia@atcc.org>
2024-03-27 12:05:49 -0400

    refactor peaks_detect
    refactored code by removing unnecessary code from peak_detect

--- peaks_detect.py
+++ peaks_detect.py
@@ -9,11 +9,6 @@ milk_data.head()
 # Get the time series from the data
 time_series = milk_data["Monthly milk production (pounds per cow)"]
 
-# Find peaks with a windows size of 4
-peaks_list_4 = pf.peak_finding(time_series, 4)
-x_peaks_4 = [x for x, y in peaks_list_4]
-y_peaks_4 = [y for x, y in peaks_list_4]
-
 # Plotting
 pf.plot_peaks(time_series, 4, "blue", "Monthly milk production\nPeaks with window size of 4")
 pf.plot_peaks(time_series, 1, "green", "Monthly milk production\nPeaks with window size of 1")
